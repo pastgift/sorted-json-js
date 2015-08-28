64ea3509bf2d7cedb526901a81763f6b23f4b2c9
pastgift <pastgift@gmail.com>
2015-08-28 13:57:54 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # sorted-json-js
 a sorting-keys-versiong for `JSON.stringify`
+
+## Quick Example
+
+```js
+sortedJSON = require('sorted-json-js');
+
+obj = {
+  d: [1.1, false, 'abc'],
+  c: {
+    cc: [1, true, function(){}],
+    cb: 2,
+    ca: 1
+  },
+  b: 2,
+  a: 1
+};
+
+console.log(sortedJSON.stringify(obj));
+
+// Output: `{"a":1,"b":2,"c":{"ca":1,"cb":2,"cc":[1,true,"<FUNCTION>"]},"d":[1.1,false,"abc"]}`
+```
+
+### Install:
+```shell
+npm install sorted-json
+```
+
+### License
+[MIT](LICENSE)
+
+[downloads-image]: http://img.shields.io/npm/dm/sorted-json.svg
+
+[npm-url]: https://npmjs.org/package/sorted-json
+[npm-image]: http://img.shields.io/npm/v/sorted-json.svg
---
+++ sorted-json.js
@@ -0,0 +1,59 @@
+'use strict'
+
+exports.stringify = function(obj, compareFunction) {
+  if (typeof obj == 'string') {
+    return '"' + obj + '"';
+  }
+  else if (typeof obj == 'number') {
+    return obj;
+  }
+  else if (typeof obj == 'boolean') {
+    return obj;
+  }
+  else if (typeof obj == 'function') {
+    return '"<FUNCTION>"';
+  }
+  else if (typeof obj == 'object') {
+    if (Array.isArray(obj)) {
+      var parts = [];
+      for (var i = 0; i < obj.length; i++) {
+        var v = exports.stringify(obj[i], compareFunction);
+        parts.push(v);
+      }
+
+      return '[' + parts.join(',') + ']';
+    }
+    else {
+      var keyList = [];
+      for (var k in obj) {
+        keyList.push(k);
+      }
+      keyList.sort(compareFunction);
+
+      var parts = [];
+      for (var i = 0; i < keyList.length; i++) {
+        var k = keyList[i];
+        var v = exports.stringify(obj[k], compareFunction);
+
+        if (typeof v == 'string') {
+          if (typeof obj[k] == 'string') {
+            parts.push('"' + k + '":"' + v + '"');
+          }
+          else if (typeof obj[k] == 'object') {
+            parts.push('"' + k + '":' + v);
+          }
+        }
+        else if (typeof v == 'number') {
+          parts.push('"' + k + '":' + v);
+        }
+        else if (typeof v == 'boolean') {
+          parts.push('"' + k + '":' + v);
+        }
+      }
+      return '{' + parts.join(',') + '}';
+    }
+  }
+  else {
+    return '"<UNKNOW>"';
+  }
+};
